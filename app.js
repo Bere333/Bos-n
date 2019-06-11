1019ec2faa8dc4251955a4a68051c1a8e0763220
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2019-06-11 06:27:51 -0500

    Se agrego la seccion de instrucciones al boton ayuda

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 //Funcionalidad de los botones
 //Funcionalidad de boton1
-document.getElementById("boton1").addEventListener("click", () => {
+document.getElementById("boton1").addEventListener("click", () => { //funcion flecha
   console.log("boton1"); //testear boton1
   const bienvenidaBoson = document.getElementById("bienvenidaBoson");/*Se creo una variable
   para la seccion llamada bienvenidaBoson*/
   bienvenidaBoson.style.display = "none"; /*La variable const Boson se hace
   desaparecer con none al hacer click en el boton1*/
 
+});
+
+document.getElementById("boton2").addEventListener("click", () => { //funcion flecha
+  console.log("boton2");//testear boton2
+  const bienvenidaBoson = document.getElementById("bienvenidaBoson")/*Se creo una variable
+  para la seccion llamada bienvenidaBoson*/
+  bienvenidaBoson.style.display = "none";/*La variable const Boson se hace
+  desaparecer con none al hacer click en el boton1*/
+})
+
+document.getElementById("boton3").addEventListener("click", () => {
+  console.log("boton3");
+  const bienvenidaBoson = document.getElementById("bienvenidaBoson");/*Se creo una variable
+  para la seccion llamada bienvenidaBoson*/
+  bienvenidaBoson.style.display = "none";/*La variable const Boson se hace
+  desaparecer con none al hacer click en el boton1*/
+  const ayuda = document.getElementById("ayudaBoson");/*Se creo una variable para la seccion de ayuda*/
+  ayudaBoson.style.display = "block"/*La variable ayuda se hace desaparecer
+   con block al hacer click en el boton3*/
 })
