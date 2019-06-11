3fc989e58a44eda5aa041c2686d0b5064e3bbebc
Brenda Berenice Ramos Hernández <elcosmossuena@gmail.com>
2019-06-11 04:52:15 -0500

    Hice desaparecer una seccion con none

--- app.js
+++ app.js
@@ -2,4 +2,9 @@
 //Funcionalidad de boton1
 document.getElementById("boton1").addEventListener("click", () => {
   console.log("boton1"); //testear boton1
+  const bienvenidaBoson = document.getElementById("bienvenidaBoson");/*Se creo una variable
+  para la seccion llamada bienvenidaBoson*/
+  bienvenidaBoson.style.display = "none"; /*La variable const Boson se hace
+  desaparecer con none al hacer click en el boton1*/
+
 })
